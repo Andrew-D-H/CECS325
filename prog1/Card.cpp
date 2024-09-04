0bc9591dfb87ac7435a97de6305213a88323ac43
Andrew-D-H <Ahua4874@gmail.com>
2024-09-04 13:52:48 -0700

    card class and header

--- prog1/Card.cpp
+++ prog1/Card.cpp
@@ -4,7 +4,7 @@
 // Due 09/18/2024
 //
 // I certify that this program is my own original work. I did not copy any part of this program from
-// any other source. I further certify that I typed each and every line of code in this program.\
+// any other source. I further certify that I typed each and every line of code in this program.
 
 #include <iostream>
 #include "Card.h"
--- prog1/Card.h
+++ prog1/Card.h
@@ -1,3 +1,11 @@
+// Andrew Hua
+// CECS 325-01
+// Prog 1 – One Card War
+// Due 09/18/2024
+//
+// I certify that this program is my own original work. I did not copy any part of this program from
+// any other source. I further certify that I typed each and every line of code in this program.
+
 #ifndef CARD_H
 #define CARD_H
 
