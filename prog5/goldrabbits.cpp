8e7526e34cc546ef2b50e6db3c48404fe565e102
Andrew-D-H@ <ahua4874@gmail.com>
2024-11-14 12:05:31 -0800

    included header

--- prog5/goldrabbits.cpp
+++ prog5/goldrabbits.cpp
@@ -1,3 +1,11 @@
+// Andrew Hua
+// CECS 325-01
+// Prog 5 – goldrabbits part 1
+// Due 11/14/2024
+//
+// I certify that this program is my own original work. I did not copy any part of this program from
+// any other source. I further certify that I typed each and every line of code in this program.
+
 #include <iostream>
 #include <time.h>
 #include <iomanip>
